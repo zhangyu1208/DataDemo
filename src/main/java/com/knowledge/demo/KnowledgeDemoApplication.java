a9cc6717e5c061152f5843ed08fcbf34b336d09e
mengqf28 <mengqf28@chinaunicom.cn>
2018-08-28 13:54:58 +0800

    init

---
+++ src/main/java/com/knowledge/demo/KnowledgeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.knowledge.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeDemoApplication.class, args);
+	}
+}
